b27b07fe14b7ff43f1583d716d0fd3689c818e7f
VickyMutai <vicky.mutai96@gmail.com>
2018-01-31 11:40:50 +0300

    import db instance

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
+from . import db
+
+class User(db.model):
+    __tablename__ = 'users'
+    id = db.Column(db.integer, primary_key = True)
+    username = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User (self.username)'
+
 class Movie:
     '''
     Movie class to define Movie Objects
--- manage.py
+++ manage.py
@@ -1,5 +1,7 @@
-from app import create_app
+from app import create_app,db
 from flask_script import Manager,Server
+from app.models import User
+
 
 #creating app instance
 
