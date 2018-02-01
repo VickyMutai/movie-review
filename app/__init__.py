ae815132c61b2146bf1356c9c77884062a329a15
VickyMutai <vicky.mutai96@gmail.com>
2018-02-01 22:10:36 +0300

    create instance

--- app/__init__.py
+++ app/__init__.py
@@ -21,6 +21,9 @@ def create_app(config_name):
     #creating the app configurations
     app.config.from_object(config_options[config_name])
 
+    #configure UploadSet
+    configure_uploads(app,photos)
+
     #registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
