6cf9094368cbfcd86a80127a23e26ecce90fac8d
VickyMutai <vicky.mutai96@gmail.com>
2018-02-01 11:20:45 +0300

    create blueprint instance of auth

--- app/__init__.py
+++ app/__init__.py
@@ -17,6 +17,9 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
     #initializing Flask Extensions
     bootstrap.init_app(app)
     db.init_app(app)
