fe6749be8f2355a1181d02c5249b0f64c526a021
VickyMutai <vicky.mutai96@gmail.com>
2018-02-02 12:09:54 +0300

    export username and password

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 export MOVIE_API_KEY='3245874243c7f8707d3fa3edaf2e81fb'
 export SECRET_KEY='0293846573289292'
+export MAIL_USERNAME = 'vicky.mutai96@gmail.com'
+export MAIL_PASSWORD = '53800272'
 
 python3.6 manage.py server
