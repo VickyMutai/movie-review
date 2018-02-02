01927c2bbf5d61459b40fa34409226c21038f43e
VickyMutai <vicky.mutai96@gmail.com>
2018-02-02 23:51:46 +0300

    remove photo profile

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 from flask import render_template,request,redirect,url_for,abort
 from . import main
 from ..request import get_movies,get_movie,search_movie
-from ..models import Review, User, PhotoProfile
+from ..models import Review, User
 from .forms import ReviewForm, UpdateProfile
 from flask_login import login_required,current_user
 from .. import db,photos
