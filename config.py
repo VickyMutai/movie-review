2136cdf8f0b8ef0949a6f6a9bec1ce130a1d2745
VickyMutai <vicky.mutai96@gmail.com>
2018-02-02 12:04:33 +0300

    configure environment variables  for email

--- config.py
+++ config.py
@@ -10,6 +10,13 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
 
+    #email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("vicky.mutai96@gmail.com")
+    MAIL_PASSWORD = os.environ.get("53800272")
+
 class ProdConfig(Config):
     '''
     Product Configuration child class
