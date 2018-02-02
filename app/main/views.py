4f6e64418b56133ecc6e3543f5d62531eb512cf2
VickyMutai <vicky.mutai96@gmail.com>
2018-02-02 22:49:22 +0300

    convert markdown to html

--- app/main/views.py
+++ app/main/views.py
@@ -5,6 +5,7 @@ from ..models import Review, User
 from .forms import ReviewForm, UpdateProfile
 from flask_login import login_required
 from .. import db,photos
+import markdown2
 
 #views
 @main.route('/')
@@ -67,6 +68,14 @@ def new_review(id):
 
     title = f'{movie.title} review'
     return render_template('new_review.html',title = title, review_form = form, movie = movie)
+
+@main.route('/review/<int:id>')
+def single_review(id):
+    review = Review.query.get(id)
+    if Review is None:
+        abort(404)
+    format_review = markdown2.markdown(review.movie_review,extras=["code-friendly","fenced-code-blocks"])
+    return render_template('review.html',review=review,format_review=format_review)
 
 @main.route('/user/<uname>')
 def profile(uname):
