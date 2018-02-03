2af5733aa123b3a8b5e3a23ee678162ef02c6bf6
VickyMutai <vicky.mutai96@gmail.com>
2018-02-03 16:26:48 +0300

    changes

---
+++ start.sh
@@ -0,0 +1,6 @@
+export MOVIE_API_KEY='3245874243c7f8707d3fa3edaf2e81fb'
+export SECRET_KEY='v1273438435924548785748qbjfdhkhjegfhkgrhvg'
+export MAIL_USERNAME='vicky.mutai96@gmail.com'
+export MAIL_PASSWORD='53800272'
+
+python3.6 manage.py server
